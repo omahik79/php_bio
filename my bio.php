38d4870e35b1f3bb43d6cda2cdd66121c648e0e1
omahik79 <46551129+omahik79@users.noreply.github.com>
2020-01-27 16:21:16 -0700

    add my bio php

---
+++ my bio.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+   
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+      
+    <title>Hello, world!</title>
+  </head>
+  <body>
+   
+  
+   
+<div class="jumbotron">
+  <h1 class="display-4"> <?php
+
+echo "Omar Almukhtar   ";
+
+?>
+</h1>
+  <p class="lead"><?php
+
+echo "40 years old  ";
+echo "from IRAQ - BAGHDAD ";
+?></p>
+  <hr class="my-4">
+  <p> <?php
+
+echo " IT Specialist with over 10+ years of experience in information technology, network configuration, database, and software development. had an Associate Degree in Cisco Network from ACC with grade 3.95 and B.SC. Computer Science / Software Engineer from a technology university with Grade 3.08, currently study MPS web and mobile application development at FHSU";
+
+?> </p>
+   
+  
+</div>
+      
+      
+   
+  </body>
+</html>
